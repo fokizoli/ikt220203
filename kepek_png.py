4174f7f92cbc2cbaec744de54dc6aa277826d49d
fokizoli <fokizoli@gmail.com>
2022-03-08 13:12:18 +0100

    kepek

--- kepek_png.py
+++ kepek_png.py
@@ -8,15 +8,17 @@ ablak1.iconphoto(True, icon)
 ablak1.config(background='black')
 elsokep = PhotoImage(file=gyoker+'light-png.png')
 cimke = Label(ablak1,
-            text='Üdvözlet!',
-            fg='#553388',
-            bg="#c3cee0",
-            font=('Arial', 45, 'bold'),
-            bd=10,
-            relief=RAISED,
-            padx=25,
-            pady=30,
-            image=elsokep,
-            compound='center').pack()
+             text='Üdvözlet!',
+             fg='#553388',
+             bg="#c3cee0",
+             font=('Arial', 45, 'bold'),
+             bd=10,
+             relief=RAISED,
+             padx=45,
+             pady=30,
+             image=elsokep,
+             compound='center'
+          ).pack()
 
 ablak1.mainloop()
+
