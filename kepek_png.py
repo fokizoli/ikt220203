338facfe298c899df532ec766c371116a0283720
fokizoli <fokizoli@gmail.com>
2022-03-09 11:35:39 +0100

    button tulajdonságok

--- kepek_png.py
+++ kepek_png.py
@@ -1,12 +1,15 @@
 from tkinter import *
+def klikk():
+       print('Klikkeltem')
 ablak1 = Tk()
 gyoker = 'C:\\Users\\fokizoltan\\gitfz\\ikt220203\\'
-ablak1.geometry('450x450')
+ablak1.geometry('600x600')
 ablak1.title('IKT Tkinter')
 icon = PhotoImage(file=gyoker+'phone-receiver-silhouette.png')
 ablak1.iconphoto(True, icon)
 ablak1.config(background='black')
-elsokep = PhotoImage(file=gyoker+'light-png.png')
+elsokep = PhotoImage(file=gyoker+'giphy.gif')
+gombkep = PhotoImage(file=gyoker+'giphy.gif', width='50', height='50')
 cimke = Label(ablak1,
              text='Üdvözlet!',
              fg='#553388',
@@ -19,6 +22,22 @@ cimke = Label(ablak1,
              image=elsokep,
              compound='center'
           ).pack()
+
+gomb = Button(ablak1,
+               text= 'Kattints!',
+               fg='blue',
+               font=('Comic Sans', 35, 'bold'),
+               bg='yellow', 
+               relief=SUNKEN,
+               bd=10,
+               command=klikk,
+               padx=12,
+               pady=12,
+               image=gombkep,
+               compound="bottom",
+               activebackground='blue',
+               activeforeground='yellow',
+               state=ACTIVE).pack()
 
 ablak1.mainloop()
 
